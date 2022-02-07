f4263c95b2fd01e3b9d550cf6a233ded739387be
Nandini-0 <97585142+Nandini-0@users.noreply.github.com>
2022-02-07 15:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastai.vision.widgets import *
 #from fastai.vision import image
 from tensorflow.keras.preprocessing import image
 import pickle
-import cv2
+from cv2 import cv2
 import numpy as np
 from PIL import Image
 import pandas as pd
