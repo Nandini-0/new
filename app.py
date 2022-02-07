3588f8efabde788f1f9dedef9b7a48987fca94d2
Nandini-0 <97585142+Nandini-0@users.noreply.github.com>
2022-02-07 23:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ labels['label_cat'] = labels['label'].astype(str)
 label_df = labels[['image', 'label_cat']]
 
 with open('dls.pkl','rb') as f:
-    dls = pickle.load(f)
+    get_x = pickle.load(f)
 
 #def get_x(r): return 'images_compressed/'+r['image'] # create path to open images in the original folder
 #def get_y(r): return r['label_cat'].split(' ') # split the labels using space as a delimitter
